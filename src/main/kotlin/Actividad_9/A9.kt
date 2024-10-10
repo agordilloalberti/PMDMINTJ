ee8c49f82bbe5115b9138bc53c543cfcae602c43
Alvarogogal <agorsua0610@g.educaand.es>
2024-10-10 19:32:35 +0200

    Desde clase

--- src/main/kotlin/Actividad_9/A9.kt
+++ src/main/kotlin/Actividad_9/A9.kt
@@ -1,10 +1,23 @@
 package Actividad_9
 
-fun main(){
-    var prota = Character("Prota")
+import java.util.Scanner
 
-    println(prota.combate())
-    println(prota.combate())
-    println(prota.combate())
-    println(prota.combate())
+fun main(){
+    val sc = Scanner(System.`in`)
+    println("Introduzca su nombre")
+    val prota = Character(sc.nextLine())
+    var op:String
+
+    do {
+        println("¿Que desea hacer?\n1.Combatir\n2.Comprobar el progreso de xp\n0.Salir")
+        op=sc.nextLine()
+
+        when (op){
+
+            "1" -> println("\n"+prota.combate())
+            "2" -> println("\n"+prota.checkXP())
+            "0" -> println("\nAdios")
+        }
+    } while (op!="0")
+
 }
--- src/main/kotlin/Actividad_9/Character.kt
+++ src/main/kotlin/Actividad_9/Character.kt
@@ -1,21 +1,26 @@
 package Actividad_9
 
 class Character(val nombre:String) {
-    var nivel = 1
-    var xp = 0
+    private var nivel = 1
+    private var xp = 0
 
     fun combate():String{
-        xp+=50
+        val cxp =(Math.random()*100).toInt()
+        xp+=cxp
         var s = ""
         if (xp>=200){
             s+=subirNivel()
         }
-        return "El personaje $nombre ha ganado 50 de xp \n$s"
+        return "El personaje $nombre ha ganado $cxp de xp \n$s"
     }
 
-    fun subirNivel():String{
+    private fun subirNivel():String{
         xp-=200
         nivel++
-        return "$nombre ha subido al nivel $nivel"
+        return "$nombre ha subido al nivel $nivel\n"
+    }
+
+    fun checkXP():String{
+        return "El personaje $nombre tiene $xp y le faltan ${200-xp} de xp para llegar al nivel ${++nivel}\n"
     }
 }
